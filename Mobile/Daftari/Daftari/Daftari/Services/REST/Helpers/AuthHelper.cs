35f65a3277f59292fe0b4d6177ec2267d7cc5b7d
Oupa Mokone <oupam@muntumi.com>
2021-12-20 16:47:46 +0200

    Mobile -  Setting up HTTP

---
+++ Mobile/Daftari/Daftari/Daftari/Services/REST/Helpers/AuthHelper.cs
@@ -0,0 +1,63 @@
+﻿using Daftari.Services.REST.ViewModels;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Daftari.Services.REST.Helpers
+{
+    public class AuthHelper
+    {
+        //public static async Task<SimpleResult> Register(UserViewModel user, CancellationTokenSource ct_Token)
+        //{
+        //    var tcs = new TaskCompletionSource<SimpleResult>();
+
+        //    try
+        //    {
+        //        var deroResult = await new RestService().PostAsync((Application.Current as App), Constants.URLs.Sign_Up, user, ct_Token.Token);
+
+        //        if (deroResult != null)
+        //        {
+        //            if (deroResult.IsSuccess)
+        //            {
+        //                tcs.SetResult(new SimpleResult { IsSuccess = true, Message = deroResult.Body.ToString() });
+        //            }
+        //            else
+        //            {
+        //                if (deroResult.ErrorMessages != null && deroResult.ErrorMessages.Any())
+        //                {
+        //                    throw new ValidationException(deroResult.ErrorMessages[0]);
+        //                }
+        //                throw new ValidationException(deroResult.Body.ToString());
+        //            }
+        //        }
+        //        else
+        //        {
+        //            throw new ValidationException("Failed to connect!");
+        //        }
+        //    }
+        //    catch (ValidationException ex)
+        //    {
+        //        tcs.SetResult(new SimpleResult { IsSuccess = false, Message = ex.Message });
+        //    }
+        //    catch (OperationCanceledException)
+        //    {
+        //        tcs.SetResult(new SimpleResult { IsSuccess = false });
+        //        // do nothing
+        //    }
+        //    catch (HttpException ex)
+        //    {
+        //        tcs.SetResult(new SimpleResult { IsSuccess = false, Message = ex.Message });
+        //    }
+        //    catch (Exception)
+        //    {
+        //        tcs.SetResult(new SimpleResult { IsSuccess = false, Message = Constants.Messages.Network_Error });
+        //    }
+        //    finally
+        //    {
+        //        //tcs.TrySetResult(new SimpleResult { IsSuccess = false, Message = Constants.Messages.Network_Error });
+        //    }
+
+        //    return await tcs.Task;
+        //}
+    }
+}
---
+++ Mobile/Daftari/Daftari/Daftari/Services/REST/ViewModels/SimpleResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daftari.Services.REST.ViewModels
+{
+    public class SimpleResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Body { get; set; }
+    }
+}
